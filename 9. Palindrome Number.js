fe2e9f6b05ec912691397e376f920828e4dffacd
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-05-01 22:02:21 -0700

    finished lc9

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,15 @@
+var isPalindrome = function(x) {
+    x = x.toString()
+    
+    var results = true;
+    var index = 0
+    while (index<x.length){
+      if (x[index] !== x[x.length-1-index]){
+        results =false
+      } else {
+        results = results
+      }
+      index++
+    }
+    return results
+};
