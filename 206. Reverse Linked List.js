8ed15a900d2860beb0ffe3c01450795b7967cf20
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-04 13:59:54 -0700

    finished lc206

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let newHead = null
+  let currentHead = head
+  let temp
+
+  while (currentHead !== null) {
+    temp = currentHead.next
+    currentHead.next = newHead
+    newHead = currentHead
+    currentHead = temp
+  }
+
+};
