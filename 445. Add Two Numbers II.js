93de802b26a75a1775798f17de661d9af6467743
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-04 15:37:28 -0700

    finished lc445

---
+++ 445. Add Two Numbers II.js
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (a, b) {
+
+  let results = new ListNode(null)
+  let currentNode = results
+
+  a = reverse(a)
+  b = reverse(b)
+  let sum = 0
+  let residual
+
+  while (a && b) {
+    residual = (a.val + b.val + sum) % 10
+    currentNode.next = new ListNode(residual)
+    sum = ((a.val + b.val + sum) - residual) / 10
+
+    a = a.next
+    b = b.next
+    currentNode = currentNode.next
+  }
+
+  while (a) {
+    residual = (a.val + sum) % 10
+    currentNode.next = new ListNode(residual)
+    sum = ((a.val + sum) - residual) / 10
+    a = a.next
+    currentNode = currentNode.next
+  }
+
+  while (b) {
+    residual = (b.val + sum) % 10
+    currentNode.next = new ListNode(residual)
+    sum = ((b.val + sum) - residual) / 10
+    b = b.next
+    currentNode = currentNode.next
+  }
+
+  if (sum > 0) {
+    currentNode.next = new ListNode(sum)
+  }
+
+  return reverse(results.next)
+}
+
+
+
+function reverse(l) {
+  let currentNode = l
+  let newHead = null
+  let temp
+
+  while (currentNode) {
+    temp = currentNode.next
+    currentNode.next = newHead
+    newHead = currentNode
+    currentNode = temp
+  }
+  return newHead
+}
