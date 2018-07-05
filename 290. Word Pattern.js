0e1adb2fa9f9e2e10154815e079b519d1a6f4214
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-05 15:19:15 -0700

    finished lc290

---
+++ 290. Word Pattern.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function (patterns, strings) {
+  patterns = patterns.split('')
+  strings = strings.split(' ')
+  if (strings.length !== patterns.length) {
+    return false
+  }
+  let patternMap = new Map();
+  let stringMap = new Map()
+  for (let i = 0; i < patterns.length; i++) {
+    if (!patternMap.get(patterns[i])) {
+      patternMap.set(patterns[i], strings[i])
+    } else {
+      if (patternMap.get(patterns[i]) !== strings[i]) {
+        return false
+      }
+    }
+
+    if (!stringMap.get(strings[i])) {
+      stringMap.set(strings[i], patterns[i])
+    } else {
+      if (stringMap.get(strings[i]) !== patterns[i]) {
+        return false
+      }
+    }
+  }
+  return true
+}
