a67e4b6cda5d1e7f01d772f727016a63aeb15db5
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-05 23:29:27 -0700

    finished lc219

---
+++ 219. Contains Duplicate II.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] === undefined) {
+      map[nums[i]] = i
+    } else {
+      if (i - map[nums[i]] <= k) {
+        return true
+      } else {
+        map[nums[i]] = i
+      }
+    }
+  }
+  return false
+};
