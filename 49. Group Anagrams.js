5e37f328c83f043badc00d56c872ca59f4770f78
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-28 21:17:25 -0700

    finished lc49

---
+++ 49. Group Anagrams.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  let map = {}
+  let i, str, sortedStr;
+
+  for (i = 0; i < strs.length; i++) {
+    str = strs[i]
+    sortedStr = str.split('').sort().join('')
+
+    if (map[sortedStr]) {
+      map[sortedStr].push(str)
+    } else {
+      map[sortedStr] = [str]
+    }
+
+  }
+
+  return Object.values(map)
+
+};
