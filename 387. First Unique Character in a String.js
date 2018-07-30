32553858206d59fc8931585ba404af192d465c0e
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-30 15:28:45 -0700

    finished lc387

---
+++ 387. First Unique Character in a String.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    let letter = s[i]
+
+    if (s.indexOf(letter) === s.lastIndexOf(letter)) {
+      return i
+    }
+  }
+  return -1
+};
