9260e270d1fcaec64be1e79a58c956cff17e64d4
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-29 22:27:22 -0700

    finished lc24

---
+++ 24. Swap Nodes in Pairs.js
@@ -0,0 +1,20 @@
+var swapPairs = function (head) {
+  if (head === null || head.next === null) {
+    return head
+  }
+
+  let dummy = new ListNode(null)
+  dummy.next = head
+  let l1 = dummy
+  let l2 = head
+
+  while (l1.next !== null && l2.next !== null) {
+    temp = l2.next.next
+    l1.next = l2.next
+    l1.next.next = l2
+    l1.next.next.next = temp
+    l1 = l2
+    l2 = l2.next
+  }
+  return dummy.next
+};
