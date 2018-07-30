c9fffd1a5808944af1c93f5618bdf5a10f203f86
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-30 16:58:58 -0700

    finished lc47

---
+++ 47. Permutations II.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums) {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1
+  }
+  console.log(map)
+  let results = []
+
+
+  const helper = (map, result) => {
+    if (result.length === nums.length) {
+      results.push(result)
+      return
+    }
+
+    for (let key in map) {
+      result.push(Number(key))
+      if (map[key] === 1) {
+        delete map[key]
+      } else {
+        map[key]--
+      }
+      helper(map, result.slice(0))
+      map[key] ? map[key]++ : map[key] = 1
+      result.pop()
+    }
+  }
+  helper(map, [])
+  return results
+};
