72a96a9929f3f8fe72b4bb8bba26eb4df421c9e8
Dylan Qian <dylan.qian.29@gmail.com>
2018-08-10 15:24:00 -0700

    finished lc15

---
+++ 15. 3Sum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  let results = []
+  if (nums.length < 3) {
+    return results
+  }
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) {
+      return results
+    }
+    if (nums[i] === nums[i - 1]) {
+      continue
+    } else {
+      let min = i + 1
+      let max = nums.length - 1
+      while (max > min) {
+        if (nums[min] === nums[min - 1] && i !== min - 1) {
+          min++
+        } else if (nums[max] === nums[max + 1]) {
+          max--
+        } else if (nums[i] + nums[min] + nums[max] === 0) {
+          results.push([nums[i], nums[min], nums[max]])
+          max--
+          min++
+        } else if (nums[i] + nums[min] + nums[max] < 0) {
+          min++
+        } else if (nums[i] + nums[min] + nums[max] > 0) {
+          max--
+        }
+
+      }
+    }
+
+  }
+  return results
+};
+
+
+
+
+threeSum([-10, -10, -5, -5, 0, 0, 0, 5, 5, 10, 10])
