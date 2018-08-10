23690cccbb04cfffba5ae63b7976c21a9a2df3c8
Dylan Qian <dylan.qian.29@gmail.com>
2018-08-10 16:08:22 -0700

    updated lc1

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -3,14 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let index = 0;
-    while (index <nums.length){
-        for (let i = index+1; i< nums.length;i++ ){
-            if (nums[index] + nums[i] === target){
-                return [index,i]
-            }
-        } 
-        index++
+var twoSum = function (nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.hasOwnProperty(target - nums[i])) {
+            map[nums[i]] = i
+        } else {
+            return [map[target - nums[i]], i]
+        }
     }
+    return null;
 };
