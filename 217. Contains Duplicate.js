763ace367cf51fcfb5d93899f0023349126acfaa
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-10-18 21:01:31 -0700

    finish 217. Contains Duplicate

---
+++ 217. Contains Duplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ const containsDuplicateHash = nums => {
+  const record ={};
+  for (let i = 0; i< nums.length; i++) {
+      if (record[nums[i]]) {
+          return true;
+      }
+      record[nums[i]] = 1;
+  }
+  return false
+};
+
+const containsDuplicateSet = nums => {
+  const set = new Set(nums);
+  return nums.length !== set.size;
+}
