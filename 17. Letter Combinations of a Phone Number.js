623fa8c38d467a23b6083587a984ac13d9b5fb60
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-10-25 23:23:12 -0700

    finish 17

---
+++ 17. Letter Combinations of a Phone Number.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+ const MAPPING=["0","1","abc","def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+
+ const letterCombinations = (digits) => {
+     let result = [];
+     const numberLength = digits.length;	
+ 
+     if(numberLength === 0){
+         return result
+     }
+ 
+   const helper = (length, string) => {
+     let letters = MAPPING[Number(digits[length])]
+     if (length === numberLength) {
+       result.push(string)
+       string = ""
+       return;
+     } else {
+       for (let i=0; i< letters.length; i++) {
+         helper(length+1, string+letters[i])
+       }
+     }
+   }
+ 
+   helper(0, "")
+ 
+   return result	
+ }
