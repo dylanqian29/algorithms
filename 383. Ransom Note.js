ba082c54bb0c8935ff7f9a0883c14fe3dc59324f
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-10-28 08:39:30 -0700

    finish 242, 383, 387

---
+++ 383. Ransom Note.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+ var canConstruct = function(ransomNote, magazine) {
+  const magazineMap = new Map();
+  
+  for (let i =0; i< magazine.length; i++) {
+      magazineMap.set(magazine[i], (magazineMap.get(magazine[i]) || 0) + 1)
+  }
+  
+  console.log(magazineMap)
+  for (let i = 0; i< ransomNote.length; i++) {
+      if (magazineMap.get(ransomNote[i]) === 0 || !magazineMap.has(ransomNote[i])) {
+          return false
+      }
+      magazineMap.set(ransomNote[i], (magazineMap.get(ransomNote[i]) - 1))
+  }
+  return true
+};
--- 387. First Unique Character in a String.js
+++ 387. First Unique Character in a String.js
@@ -12,3 +12,26 @@ var firstUniqChar = function (s) {
   }
   return -1
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var firstUniqChar = function(s) {
+  //can do 2 iteration in order to check duplicate o(n^2)
+  
+  const hashMap = new Map()
+  
+  for (let i=0; i<s.length; i++) {
+      hashMap.set(s[i], (hashMap.get(s[i]) || 0)+1)
+  }
+  
+  console.log(hashMap)
+  
+  for (let i=0; i<s.length; i++) {
+      if (hashMap.get(s[i]) === 1) {
+          return i
+      }
+  }
+  return -1;
+};
