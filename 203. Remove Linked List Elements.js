6c7d332b61c315004053b1c84e722a55d1d64841
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-10-28 22:50:00 -0700

    finish 203

--- 203. Remove Linked List Elements.js
+++ 203. Remove Linked List Elements.js
@@ -33,3 +33,41 @@ var removeElements = function (head, val) {
   }
   return head;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  if (!head) {
+    return null;
+  }
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let pointer = dummy.next;
+  let last = dummy;
+
+  while (pointer) {
+    if (pointer.val === val) {
+      if (pointer.next) {
+        pointer = pointer.next;
+      } else {
+        last.next = null;
+        pointer = pointer.next;
+      }
+    } else {
+      last.next = pointer;
+      last = last.next;
+      pointer = pointer.next;
+    }
+  }
+  return dummy.next;
+};
