4c59fc09b51fbbaebeaca70fb2bd21b32898eda1
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-10-29 10:02:15 -0700

    update recursive solution for 617

--- 617. Merge Two Binary Trees.js
+++ 617. Merge Two Binary Trees.js
@@ -11,6 +11,7 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
+// iterative
 var mergeTrees = function (root1, root2) {
   if (!root1) {
     return root2;
@@ -45,5 +46,35 @@ var mergeTrees = function (root1, root2) {
       next[0].right = next[0].right;
     }
   }
+  return root1;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+// recursive
+var mergeTrees = function (root1, root2) {
+  if (!root1) {
+    return root2;
+  }
+
+  if (!root2) {
+    return root1;
+  }
+
+  root1.val = root1.val + root2.val;
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right = mergeTrees(root1.right, root2.right);
+
   return root1;
 };
