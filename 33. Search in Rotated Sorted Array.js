43eecd734b2114b449f3d7546ca6b2771da6ae4d
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-11-04 23:06:27 -0700

    finish 33

---
+++ 33. Search in Rotated Sorted Array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid, midVal;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    midVal = nums[mid];
+    if (target === nums[mid]) {
+      return mid;
+    }
+    //left side is sorted
+    if (nums[mid] >= nums[left]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      //right side is sorted
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
